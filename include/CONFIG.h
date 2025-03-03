aa090494c80af9a04a0208984d16a93b76712682
dxhoangsteve <sterbe2k4@gmail.com>
2025-03-03 08:45:01 +0700

    modifile: CONFIG.h add mqtt value

--- include/CONFIG.h
+++ include/CONFIG.h
@@ -3,3 +3,10 @@
 #define RELAYPIN 4
 
 #define LEDPIN 2
+
+#define MQTT_BROKER "127.0.0.1"
+#define MQTT_PORT 1883
+#define MQTT_USER ""
+#define MQTT_PASS ""
+#define SUB_TOPIC "home/relay/control"
+#define PUB_TOPIC "home/relay/status"
